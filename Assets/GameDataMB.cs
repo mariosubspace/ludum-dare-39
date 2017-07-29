fbe33de76c3b83074aeab4ecd137e4a913299a1d
Mario Gutierrez <0x4d4147@gmail.com>
2017-07-29 01:17:58 -0500

    Initial commit, basic stuff for LD 39

---
+++ Assets/GameDataMB.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDataMB : MonoBehaviour
+{
+    [HideInInspector]
+    public float powerLevel;
+
+    [HideInInspector]
+    public int currentNumberOfPowerOrbs;
+
+    public int maxNumberOfPowerOrbs = 5;
+
+
+}
