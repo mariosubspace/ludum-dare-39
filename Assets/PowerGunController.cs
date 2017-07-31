35ca1f1fcc4779f2ed1538d86651830d24b826b8
Mario Gutierrez <0x4d4147@gmail.com>
2017-07-31 09:19:32 -0500

    Added raycasting from gun, reticle changes color when pointing a lamp.

---
+++ Assets/PowerGunController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerGunController : MonoBehaviour
+{
+    public Image reticle;
+    Ray r = new Ray();
+    public float maxDistance = 10f;
+    public LayerMask layers;
+    Transform exitPoint;
+    Transform hitRaycastStart;
+
+    private void Awake()
+    {
+        exitPoint = transform.Find("Exit Point");
+        hitRaycastStart = transform.Find("Hit Raycast Start");
+    }
+
+    private void Update()
+    {
+        RaycastHit hit;
+        r.origin = hitRaycastStart.position;
+        r.direction = hitRaycastStart.up;
+
+        Debug.DrawRay(exitPoint.position, r.direction, Color.blue);
+        if (Physics.Raycast(r, out hit, maxDistance, layers))
+        {
+            reticle.color = Color.red;
+        }
+        else
+        {
+            reticle.color = Color.white;
+        }
+    }
+}
