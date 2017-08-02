d04eccd5307d21b24ca55f2be4b092632b2989b3
Mario Gutierrez <0x4d4147@gmail.com>
2017-08-02 02:13:58 -0500

    Added NavMesh, ghosts are now agents and follow the player, ghost colliders added.

---
+++ Assets/TargetPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TargetPlayer : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+    Player player;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        player = FindObjectOfType<Player>();
+    }
+
+    private void Update()
+    {
+        navMeshAgent.destination = player.transform.position;
+    }
+}
